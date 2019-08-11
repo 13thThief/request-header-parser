d5b06a769a5cf5952b93508631284356504fb14d
13thThief <manpreet.plus@gmail.com>
2019-08-11 22:41:13 +0530

    Fix ipaddress parsing

--- api/index.js
+++ api/index.js
@@ -1,22 +1,20 @@
 'use strict';
 
 let getInfo = function(req){
-  let ip;
-  let xFor = req.headers['x-forwarded-for'];
-  let remoteAddress = req.connection.remoteAddress;
+  let ip = req.headers['x-forwarded-for'].split(',')[0];
+  // let xFor = req.headers['x-forwarded-for'];
+  // let remoteAddress = req.connection.remoteAddress;
   let language = req.headers['accept-language'];
   let software = req.headers['user-agent'];
 
-  if(xFor)
-    ip = xFor;
-  else ip = remoteAddress;
+  // ip = xFor ? xFor : remoteAddress;
 
   let body = {
     ipaddress: ip,
     language: language,
     software: software
   }
-  return body
+  return body;
 }
 
 module.exports = { 
